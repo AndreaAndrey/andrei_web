fa8645d9c7da961e91259141cb3a327ab04123b5
Luca Angioloni <lucaangioloni@gmail.com>
2021-01-01 20:57:33 +0100

    fixed modal viewer

--- src/components/ImgList.vue
+++ src/components/ImgList.vue
@@ -3,7 +3,7 @@
   <div style="display:inline-block; margin: auto;">
     <div class="tag-list">
         <h5>Common Tags</h5>
-        <span class="tag" v-for="tag in tag_list.slice(0,10)" v-bind:key="tag.tag" v-bind:class="{ active_tag: tag_search.split(' ').includes(tag.tag)}" @click="add_tag_search(tag.tag)">
+        <span class="tag" v-for="tag in tag_list.slice(0,10)" :key="tag.tag" :class="{ active_tag: tag_search.split(' ').includes(tag.tag)}" @click="add_tag_search(tag.tag)">
           {{tag.tag}}
         </span>
     </div>
@@ -13,29 +13,13 @@
   </div>
   <div style="width: 100%"><hr></div>
 
-    <modal ref="modalName">
-      <template v-slot:header>
-        <h1>Modal title</h1>
-      </template>
+  <modal v-if="showModal" @close="showModal = false" :panel_obj="panel_obj"></modal>
 
-      <template v-slot:body>
-        <img v-bind:src="img_panel">
-      </template>
-
-      <template v-slot:footer>
-        <div>
-          <button @click="$refs.modalName.closeModal()">Cancel</button>
-          <button @click="$refs.modalName.closeModal()">Save</button>
-        </div>
-      </template>
-    </modal>
   <loading :active="isLoading"
         :can-cancel="false"></loading>
   <div class="gallery" v-for="img in images" :key="img.name">
-    <img v-bind:src="img.img_data" @click="view_image(img)">
-    <div class="desc">{{ img.name }} - {{ img.size }}</div>
-    <button @click="addTag(img)">Add Tag</button>
-    <input :id="'input_tag'+img.name" placeholder="edit me">
+    <img :src="img.media_data" @click="view_image(img)">
+    <div class="desc">{{ img.name }}</div>
   </div>
 </template>
 
@@ -49,8 +33,6 @@ import Modal from './MediaPanel';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
-import firebase from '@/firebaseinit.js';
-
 // Transform callback based method into proper async function with Promises
 let download_file = async (file) => {
   return new Promise(
@@ -62,31 +44,22 @@ let download_file = async (file) => {
     }
   );
 };
-//to be moved where it belongs
-function filename_2_firekey (filename) {
-  const encoded = window.btoa(filename)
-    return encoded;
-}
-//to be moved where it belongs
-// function firekey_2_filename (hash) {
-//   const decoded = window.atob(hash)
-
-//     return decoded;
-// }
 
 export default {
   name: "ImgList",
   data() {
     return {
       images: [],
-      img_panel: '',
+      panel_obj: {},
+      showModal: false,
       isLoading: true,
       fullPage: true,
       page: 1,
       page_select: 1,
       tag_search: '',
       per_page: 12,
-      filter_condition: null
+      filter_condition: null,
+      type_condition: null
     }
   },
   computed: {
@@ -94,7 +67,17 @@ export default {
       return this.file_list.length;
     },
     file_list: function () {
-      return this.$store.getters.get_list(this.filter_condition);
+      var total_filter = null;
+      if(this.filter_condition || this.type_condition){
+        if(this.filter_condition && this.type_condition){
+          total_filter = file => this.filter_condition(file) && this.type_condition(file);
+        } else if(this.filter_condition) {
+          total_filter = this.filter_condition;
+        } else {
+          total_filter = this.type_condition;
+        }
+      }
+      return this.$store.getters.get_list(total_filter);
     },
     tag_list: function () {
       return this.$store.state.tag_list;
@@ -140,18 +123,18 @@ export default {
 
       Promise.all(children_list.map(async x => {
           let extension = x.name.split('.').pop();
-          var img_data = '';
+          var media_data = '';
           if(extension == 'm4a' || extension == 'acc'|| extension == 'mp3'){
-            img_data = this.audio_icon;
+            media_data = this.audio_icon;
           } else if(extension == 'mp4'){
-            img_data = this.video_icon;
+            media_data = this.video_icon;
           } else if(extension == 'pdf'){
-            img_data = this.pdf_icon;
+            media_data = this.pdf_icon;
           } else {
-            img_data = await download_file(x);
-            img_data = 'data:image/gif;base64,'+img_data;
+            media_data = await download_file(x);
+            media_data = 'data:image/gif;base64,'+media_data;
           }
-          return {name : x.name, size : x.size, timestamp : x.timestamp, img_data : img_data, obj: x}
+          return {name : x.name, size : x.size, timestamp : x.timestamp, media_data : media_data, obj: x}
         })
       ).then((img_list) => {
         this.isLoading = false;
@@ -160,10 +143,8 @@ export default {
     },
     view_image(img){
       console.log(img.name);
-
-      this.img_panel= img.img_data;
-
-      this.$refs.modalName.openModal();
+      this.panel_obj= img;
+      this.showModal = true;
     },
     search_by_tag(){
       this.page = 1; // reset page to the first
@@ -191,33 +172,9 @@ export default {
     delete_search(){
       this.tag_search = "";
       this.search_by_tag();
-    },
-    addTag(img){
-      let input_tag = document.getElementById('input_tag'+img.name).value
-
-      //add the tag to the datbase tag_list
-      let tag_list_ref = firebase.database().ref("/tag_list");
-      tag_list_ref.child(filename_2_firekey(input_tag)).set({
-        tag_name: input_tag
-      });
-
-      //tagging_db is pointing to the path /tagging_db in DB
-      let tagging_db = firebase.database().ref("/tagging_db");
-
-      //create or replaces a path in /tagging_db/$tag_input/encoded(img.name)
-      var tagref = tagging_db.child(input_tag).child(filename_2_firekey(img.name));
-      //Setting data to that path
-      tagref.set({
-        filename: img.name,
-        size: img.size
-      });
-      console.log('Added '+ img.name + ' to tag ' + input_tag)
-
-      document.getElementById('input_tag'+img.name).value='';
     }
   }
 }
-
 </script>
 
 <style>
@@ -251,9 +208,6 @@ div.desc {
   border: 1px solid #aa8;
   margin: 10px;
   padding: 10px;
-  ul > * {
-    display: inline-block;
-  }
 }
 
 .tag {
@@ -262,19 +216,6 @@ div.desc {
   border-radius:5px;
   background-color: #9bd;
   color: #ffff;
-  .delete-btn{
-    $db-size: 18px;
-    $db-color: $color-neg;
-    width: $db-size;
-    height: $db-size;
-    padding: 0 5px 0 4px;
-    color: $db-color;
-    border: 1px solid $db-color;
-    border-radius: 25%;
-    background: none;
-    font-size: 1em;
-    line-height: $db-size - 6;
-  }
 }
 
 .active_tag {
--- src/components/MediaPanel.vue
+++ src/components/MediaPanel.vue
@@ -1,27 +1,37 @@
 <template>
-  <transition name="fade">
-    <div v-if="show">
-      <div @click="closeModal()"/>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
 
-      <div>
-        <div>
-          <slot name="header"/>
-          <button type="button" @click="closeModal()">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
-              <path
-                fill="currentColor"
-                d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
-              ></path>
-            </svg>
-          </button>
-        </div>
+          <div class="modal-header">
+            <h3>{{panel_obj.name}}</h3>
+          </div>
 
-        <div>
-          <slot name="body"/>
-        </div>
+          <loading :active="isLoading" :can-cancel="false" id="modal_loader"></loading>
 
-        <div>
-          <slot name="footer"/>
+          <div v-if="!isLoading" class="modal-body">
+            <img class="media_tag" v-if="file_type == 'img'" :src="panel_obj.media_data">
+
+            <video class="media_tag" v-if="file_type == 'video'" controls>
+              <source :src="media_data" type="video/mp4">
+            Your browser does not support the video tag.
+            </video>
+
+            <audio v-if="file_type == 'audio'" controls>
+              <source :src="media_data" type="audio/mpeg">
+            Your browser does not support the audio element.
+            </audio>
+          </div>
+
+          <div class="modal-footer">
+            Tag list
+            <input id="input_tag" placeholder="Tag list" v-model="tag_list">
+            <button @click="addTag">Save</button>
+            <button class="modal-default-button" @click="$emit('close')">
+              Close
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -29,22 +39,170 @@
 </template>
 
 <script>
+import Loading from 'vue3-loading-overlay';
+import firebase from '@/firebaseinit.js';
+
+//to be moved where it belongs
+function filename_2_firekey (filename) {
+  const encoded = window.btoa(filename)
+    return encoded;
+}
+//to be moved where it belongs
+// function firekey_2_filename (hash) {
+//   const decoded = window.atob(hash)
+
+//     return decoded;
+// }
+
 export default {
   name: "Modal",
+  props: {
+    panel_obj: Object,
+  },
   data() {
     return {
-      show: false
+      media_data: null,
+      isLoading: false,
+      tag_list: ''
     };
   },
-  methods: {
-    closeModal() {
-      this.show = false;
-      document.querySelector("body").classList.remove("overflow-hidden");
+  components: {
+    Loading
+  },
+  computed: {
+    file_ext(){
+      return this.panel_obj.name.split('.').pop();
     },
-    openModal() {
-      this.show = true;
-      document.querySelector("body").classList.add("overflow-hidden");
+    file_type(){
+      let extension = this.file_ext;
+      if(extension == 'm4a' || extension == 'acc'|| extension == 'mp3'){
+        return 'audio';
+      } else if(extension == 'mp4'){
+        return 'video';
+      } else if(extension == 'pdf'){
+        return 'pdf';
+      } else {
+        return 'img';
+      }
+    }
+  },
+  watch: {
+    'panel_obj.name': function(n, o) {
+      console.log("Whatcher");
+      console.log('Prop changed: ', n, ' | was: ', o);
+      if(this.file_type != 'img'){
+        this.isLoading = true;
+        console.log("W Video");
+        this.panel_obj.obj.download((err, data) => {
+          if (err){
+            this.media_data = '';
+          }
+          this.media_data = data.toString("base64");
+          this.isLoading = false;
+        });
+      }
+    }
+  },
+  methods: {
+    addTag(){
+      //add the tag to the datbase tag_list
+      let tag_list_ref = firebase.database().ref("/tag_list");
+      tag_list_ref.child(filename_2_firekey(this.tag_list)).set({
+        tag_name: this.tag_list
+      });
+
+      //tagging_db is pointing to the path /tagging_db in DB
+      let tagging_db = firebase.database().ref("/tagging_db");
+
+      //create or replaces a path in /tagging_db/$tag_input/encoded(img.name)
+      var tagref = tagging_db.child(this.tag_list).child(filename_2_firekey(this.panel_obj.name));
+      //Setting data to that path
+      tagref.set({
+        filename: this.panel_obj.name,
+        size: this.panel_obj.size
+      });
+      console.log('Added '+ this.panel_obj.name + ' to tag ' + this.tag_list)
+
+      this.tag_list = ''
     }
   }
 };
 </script>
+
+<style scoped>
+.media_tag {
+  max-width:80%;
+  max-height:80%;
+  /* height:auto !important; */
+  object-fit: contain;
+}
+
+#modal_loader{
+  z-index: 9999;
+}
+
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  width: 90%;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+  margin-top: 0;
+  color: #42b983;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+
+.modal-default-button {
+  display: block;
+  margin-top: 1rem;
+}
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+
+.modal-enter {
+  opacity: 50;
+}
+
+.modal-leave-active {
+  opacity: 50;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
