38cf4422720380562b5f9890b775e122b7e7a03b
Luca Angioloni <lucaangioloni@gmail.com>
2021-01-12 19:22:00 +0100

    added cache to save data and time

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import { File } from '@/../node_modules/megajs/dist/main.browser-es.js';
 // import { File } from 'megajs';
 import LRU from 'lru-cache';
 
-const cache_options = { max: 500000000 // 500 MB
+const cache_options = { max: 100000000 // 100 MB
   , length: function (n, key) { return n.length + key.length} // string length is in bytes
   , maxAge: 1000 * 60 * 60 }
 
